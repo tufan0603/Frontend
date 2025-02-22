16b763bd41294c0ced5da330479816f97d1dc742
Tufan Mandal <tufanm843@gmail.com>
2025-02-22 13:00:37 +0530

    Add project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import "../css/project.css";
+
+import React, { useState } from "react";
+import { UploadCloud } from "lucide-react";
 
 function Projects() {
-  return <div>Projects</div>;
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    }
+  };
+
+  return (
+    <div className="container-fluid projects-container-f ">
+      <div className="container">
+        <form className="row">
+          <div className="col-12">
+            <h1 className="add-projects">Add Projects</h1>
+          </div>
+          <div className=" col-12 file d-flex flex-column">
+            <label
+              htmlFor="file-upload"
+              className="cursor-pointer d-flex flex-column align-items-center justify-content-center"
+            >
+              <UploadCloud size={48} className="text-primary" />
+              <span className="mt-2 text">
+                {fileName || "Click to upload or drag and drop a image"}
+              </span>
+              <input
+                id="file-upload"
+                type="file"
+                className="d-none"
+                onChange={handleFileChange}
+              />
+            </label>
+          </div>
+
+          <div className="col-lg-6 project-input">
+            <label htmlFor="title" className="label">
+              Project Name
+            </label>
+            <input type="text" name="title" placeholder="Name" />
+          </div>
+          <div className="col-lg-6 project-input">
+            <label htmlFor="github" className="label">
+              Github Link
+            </label>
+            <input type="text" name="github" placeholder="Link" />
+          </div>
+          <div className="col-12 project-input">
+            <label htmlFor="smallDescription" className="label">
+              Giva a short overview of the Project
+            </label>
+            <textarea
+              type="text"
+              rows="3"
+              cols="50"
+              contact
+              name="smallDescription"
+              id="smallDescription"
+              placeholder="overview"
+              className="project-input"
+              style={{ height: "100px" }}
+            />
+            {/* <input type="text" name="smallDescription" /> */}
+          </div>
+          <div className="col-12 project-input">
+            <label htmlFor="bigDescription" className="label">
+              Give a brief description.
+            </label>
+            <textarea
+              type="text"
+              rows="5"
+              cols="50"
+              contact
+              name="bigDescription"
+              id="bigDescription"
+              placeholder="description"
+              className="project-input"
+              style={{ height: "150px" }}
+            />
+          </div>
+
+          <div className="col-lg-12 addSkillBtnCol">
+            <button type="submit" className="addProjectBtn">
+              Add Project
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
